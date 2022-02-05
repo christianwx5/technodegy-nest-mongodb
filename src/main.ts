9bf7bbfdac8ae8befb202061b751d6065ba884ae
christianwx5 <79468653+christianwx5@users.noreply.github.com>
2022-02-05 09:42:02 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const port = process.env.PORT || 3006;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+bootstrap();
